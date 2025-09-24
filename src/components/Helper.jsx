d22292b6f753b71553e5044b09aea8158c3bf007
Sakshi Jagwani <jagwanisakshi4@gmail.com>
2025-09-24 22:27:32 +0530

    Add files via upload

---
+++ src/components/Helper.jsx
@@ -0,0 +1,33 @@
+//THIS FUNC. CONVERTS WINDDIRECTION GIVEN IN DEGREES
+export const getWindDirection = (deg) => {
+
+    if (deg > 337.5 || deg <= 22.5) return 'N';
+    if (deg > 22.5 && deg <= 67.5) return 'NE';
+    if (deg > 67.5 && deg <= 112.5) return 'E';
+    if (deg > 112.5 && deg <= 157.5) return 'SE';
+    if (deg > 157.5 && deg <= 202.5) return 'S';
+    if (deg > 202.5 && deg <= 247.5) return 'SW';
+    if (deg > 247.5 && deg <= 292.5) return 'W';
+    if (deg > 292.5 && deg <= 337.5) return 'NW';
+};
+
+//THIS FUNC. CHECKS & RETURN HUMIDITY LEVELS
+export const getHumidityValue = (humidity) => {
+    if (humidity < 30) return 'Low';
+    if (humidity < 60) return 'Moderate';
+    return 'High';
+};
+
+// THIS FUNC. CONVERTS METERS TO KM
+export const getVisibilityValue = (visibility) => {
+    const km = visibility / 1000;
+    return `${km.toFixed(1)} km`;
+};
+
+// THIS FUNC. CONVERTS TEMP. (CELSIUS TO FARHENEIT) UPTO 1 DECIMAL PLACE
+export const convertTemperature = (temp, unit) => {
+    if (unit === 'F') {
+        return (temp * 9 / 5 + 32).toFixed(1);
+    }
+    return temp.toFixed(1);
+};
